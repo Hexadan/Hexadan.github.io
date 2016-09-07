e54c74601f81fa183863488a41a817117f286114
Danny <dannyjm1996@gmail.com>
2016-09-06 23:32:01 -0400

    New JS and info added
    JavaScript function to display the last date the document was edited
    along with some new information about the company.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-﻿
+function lastChanged()
+ {
+	var lastModDate = document.lastModified;
+	var lastModDate = lastModDate.substring(0,10);
+	displayDateLast.innerHTML = "This document was last modified "+lastModDate+".</h6>";
+}
