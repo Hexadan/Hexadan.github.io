a22872f00d51c3d832ea3590764410c539ce0ede
Danny <dannyjm1996@gmail.com>
2016-09-19 11:42:57 -0400

    JavaScript Calculations and Updated XML
    calculations for each product and updated XML

--- js/script.js
+++ js/script.js
@@ -5,8 +5,23 @@ Desc: JavaScript for website
 */
 
 function lastChanged()
- {
+{
 	var lastModDate = document.lastModified;
 	var lastModDate = lastModDate.substring(0,10);
 	displayDateLast.innerHTML = "<h6>&copy;2016 Zoom Tunes</h6><h6> This document was last modified "+lastModDate+".</h6>";
+}
+
+function fproductTotal(total)
+{
+	var tempSubtotal = (document.getElementById("productQuantity").selectedIndex + 1) * total;
+
+  document.getElementById("productSubtotal").value = "$" + tempSubtotal;
+
+	var tempTax = (tempSubtotal * 0.065);
+
+	document.getElementById("productTax").value = "$" + tempTax;
+
+	document.getElementById("productSandH").value = "$5";
+
+	document.getElementById("productTotal").value = "$" + (tempSubtotal + tempTax + 5);
 }
