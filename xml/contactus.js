a78965330f78b1a0be3a23cd8c47e862e497cdf5
Danny <dannyjm1996@gmail.com>
2016-09-26 11:16:13 -0400

    fixed variable name

--- xml/contactus.js
+++ xml/contactus.js
@@ -27,15 +27,15 @@ function FindClub()
 
   for (i = 0; i < x.length; i++)
   {
-    Employee = x[i].getElementsByTagName("CONTACT")[0].childNodes[0].nodeValue;
-    if (Employee.toUpperCase().indexOf(SearchString) == 0)
+    Contact = x[i].getElementsByTagName("CONTACT")[0].childNodes[0].nodeValue;
+    if (Contact.toUpperCase().indexOf(SearchString) == 0)
     {
       Dept = x[i].getElementsByTagName("DEPT")[0].childNodes[0].nodeValue;
       Title = x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue;
       Phone = x[i].getElementsByTagName("PHONE")[0].childNodes[0].nodeValue;
       Email = x[i].getElementsByTagName("EMAIL")[0].childNodes[0].nodeValue;
       outputResult += "<span style='color:maroon'>Club:&nbsp;</span>"
-      + "<span style='font-style:italic'>" + Contact+ " </span>"
+      + "<span style='font-style:italic'>" + Contact + " </span>"
       + "<br/><span style='color:maroon'>Dept:&nbsp;</span>" + Dept
             + "<br/><span style='color:maroon'>Title:&nbsp;</span>" + Title
       + "<br/><span style='color:maroon'>Phone:&nbsp;</span>" + Phone
