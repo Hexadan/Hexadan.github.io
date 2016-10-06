0bc7c90fb981b35eb33e6751c8092c1d015d9084
Danny <dannyjm1996@gmail.com>
2016-10-05 23:11:37 -0400

    automated navbar - WIP

---
+++ js/navbar.js
@@ -0,0 +1,24 @@
+
+
+var navBarContent = "";
+
+function createNavbar()
+{
+  navBarContent += "<nav class='text-center'>" +
+  "<a class='btn btn-primary' href='aboutus.html'>About Us</a>" +
+  "<a class='btn btn-primary' href='#'>Login/Registration</a>" +
+  "<a class='btn btn-primary' href='productorder.html'>Product Order</a>" +
+  "<a class='btn btn-primary' href='productorder.html'>Product Order</a>" +
+  "<a class='btn btn-primary' href='#'>Catalog</a>" +
+  "<a class='btn btn-primary' href='#'>Inventory</a>"  ;
+
+}
+
+/*
+
+
+  <a class='btn btn-primary' href='#'>Shopping Cart</a>
+  <a class='btn btn-primary' href='companydirectory.html'>Company Directory</a>
+  <a class='btn btn-primary' href='contactus.html'>Contact Us</a>
+</nav>
+*/
