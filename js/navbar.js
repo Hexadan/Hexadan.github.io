76dd74b4a7a08ba493816e53f9737bd9d6207a52
Danny <dannyjm1996@gmail.com>
2016-10-08 20:29:14 -0400

    automated navbar - finished

--- js/navbar.js
+++ js/navbar.js
@@ -4,21 +4,14 @@ var navBarContent = "";
 
 function createNavbar()
 {
-  navBarContent += "<nav class='text-center'>" +
-  "<a class='btn btn-primary' href='aboutus.html'>About Us</a>" +
-  "<a class='btn btn-primary' href='#'>Login/Registration</a>" +
-  "<a class='btn btn-primary' href='productorder.html'>Product Order</a>" +
-  "<a class='btn btn-primary' href='productorder.html'>Product Order</a>" +
-  "<a class='btn btn-primary' href='#'>Catalog</a>" +
-  "<a class='btn btn-primary' href='#'>Inventory</a>"  ;
-
+  document.getElementById("navBarDisplay").innerHTML = "<nav class='text-center'>" +
+  "<a class='btn btn-primary' style='margin-left:2px; margin-right:2px;' href='aboutus.html'>About Us</a>" +
+  "<a class='btn btn-primary' style='margin-left:2px; margin-right:2px;' href='#'>Login/Registration</a>" +
+  "<a class='btn btn-primary' style='margin-left:2px; margin-right:2px;' href='productorder.html'>Product Order</a>" +
+  "<a class='btn btn-primary' style='margin-left:2px; margin-right:2px;' href='#'>Catalog</a>" +
+  "<a class='btn btn-primary' style='margin-left:2px; margin-right:2px;' href='#'>Inventory</a>" +
+  "<a class='btn btn-primary' style='margin-left:2px; margin-right:2px;' href='#'>Shopping Cart</a>" +
+  "<a class='btn btn-primary' style='margin-left:2px; margin-right:2px;' href='companydirectory.html'>Company Directory</a>" +
+  "<a class='btn btn-primary' style='margin-left:2px; margin-right:2px;' href='contactus.html'>Contact Us</a>" +
+  "</nav>" ;
 }
-
-/*
-
-
-  <a class='btn btn-primary' href='#'>Shopping Cart</a>
-  <a class='btn btn-primary' href='companydirectory.html'>Company Directory</a>
-  <a class='btn btn-primary' href='contactus.html'>Contact Us</a>
-</nav>
-*/
