cd40e1d047b5c879cb92dd375db0a3b2f7d985f0
Danny <dannyjm1996@gmail.com>
2016-10-08 21:25:24 -0400

    JavaScript function + webpage updates
    - combined navbar.js with script.js
    - added fixed div on each page for navbar generation
    - new layout for date last modified and email

--- js/script.js
+++ js/script.js
@@ -4,24 +4,42 @@ Date: 9/12/2016
 Desc: JavaScript for website
 */
 
-function lastChanged()
+function docLastModified()
 {
-	var lastModDate = document.lastModified;
-	var lastModDate = lastModDate.substring(0,10);
+	//displays the date the webpage was last modified
+	var docLastMod = document.lastModified;
+	var lastModDate = docLastMod.substring(0,10);
 	displayDateLast.innerHTML = "<h6>&copy;2016 Zoom Tunes</h6><h6> This document was last modified "+lastModDate+".</h6>";
 }
 
-function fproductTotal(total, shipping)
+function generateProductTotal(total, shipping)
 {
-	var tempSubtotal = (document.getElementById("productQuantity").selectedIndex + 1) * total;
+	var e = document.getElementById("productQuantity");
+	var tempSubtotal = (e.options[e.selectedIndex].text * total);
 
-  document.getElementById("productSubtotal").value = "$" + tempSubtotal;
+	//displays the total cost of the order before tax
+  document.getElementById("productSubtotal").value = "$" + tempSubtotal.toFixed(2);
 
+	//calculates tax of order
 	var tempTax = (tempSubtotal * 0.065);
 
+	//displays the tax, shipping total, and order total
 	document.getElementById("productTax").value = "$" + tempTax;
-
 	document.getElementById("productSandH").value = "$" + shipping;
-
 	document.getElementById("productTotal").value = "$" + (tempSubtotal + tempTax + 5);
+}
+
+//embeds navbar via JS
+function createNavbar()
+{
+  document.getElementById("navBarDisplay").innerHTML = "<nav class='text-center'>" +
+  "<a class='btn btn-primary' style='margin-left:2px; margin-right:2px;' href='aboutus.html'>About Us</a>" +
+  "<a class='btn btn-primary' style='margin-left:2px; margin-right:2px;' href='#'>Login/Registration</a>" +
+  "<a class='btn btn-primary' style='margin-left:2px; margin-right:2px;' href='productorder.html'>Product Order</a>" +
+  "<a class='btn btn-primary' style='margin-left:2px; margin-right:2px;' href='#'>Catalog</a>" +
+  "<a class='btn btn-primary' style='margin-left:2px; margin-right:2px;' href='#'>Inventory</a>" +
+  "<a class='btn btn-primary' style='margin-left:2px; margin-right:2px;' href='#'>Shopping Cart</a>" +
+  "<a class='btn btn-primary' style='margin-left:2px; margin-right:2px;' href='companydirectory.html'>Company Directory</a>" +
+  "<a class='btn btn-primary' style='margin-left:2px; margin-right:2px;' href='contactus.html'>Contact Us</a>" +
+  "</nav>" ;
 }
